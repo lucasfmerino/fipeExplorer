e450f2ca88520ed71c5cdde3e15b41547400f8e8
lucasfmerino <lucasfmerino@gmail.com>
2024-01-19 08:45:14 -0300

    first commit

---
+++ src/main/java/br/com/projects/FipeExplorer/FipeExplorerApplication.java
@@ -0,0 +1,13 @@
+package br.com.projects.FipeExplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projects/FipeExplorer/FipeExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projects.FipeExplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
